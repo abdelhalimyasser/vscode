ceb9d0de8940d11f56e07e1d0f8e8a0fc624a74a
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2026-04-27 19:21:32 +0300

    toz yala

--- finished/services/AuthService.php
+++ finished/services/AuthService.php
@@ -2,7 +2,34 @@
 
 namespace services;
 
+use model\User;
+
+/**
+ * Class AuthService
+ *
+ * Represents the authentication service responsible for handling user authentication and authorization processes.
+ * This service include methods for user login, registration, password cookies and seesions management.
+ * It serves as a central point for managing user authentication and ensuring secure access to protected resources.
+ *
+ * @package services
+ * @version 1.0
+ * @since 27-04-2026
+ * @author Abdelhalim Yasser
+ */
 class AuthService
 {
+    public function login($email, $password)
+    {
+        // Implement login logic here
+    }
 
+    public function register(User $user)
+    {
+        // Implement registration logic here
+    }
+
+    public function forgetPassword(string $email, string $countryCode, string $phoneNumber)
+    {
+        // Implement forget password logic here
+    }
 }
